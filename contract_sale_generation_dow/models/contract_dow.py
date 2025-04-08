81eb6a49f7fe8ed71657cf5016a498edf458b5b3
dlb002 <lbdani96@gmail.com>
2025-04-08 09:12:25 +0200

    modify : Translate

--- contract_sale_generation_dow/models/contract_dow.py
+++ contract_sale_generation_dow/models/contract_dow.py
@@ -9,5 +9,5 @@ class ContractDow(models.Model):
     _name = "contract.dow"
     _description = "Contract Days of the Week"
 
-    name = fields.Char(string="Days of the Week", required=True)
+    name = fields.Char(string="Days of the week", required=True)
     dow = fields.Integer(required=True)
